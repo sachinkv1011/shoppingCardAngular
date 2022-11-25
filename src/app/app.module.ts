e56adf10f4472ee4a6ef4348d1fa323f32f56fe3
SACHIN K V <sachin.kv.1011@gmail.com>
2022-11-25 18:18:54 +0530

    navgation and some editing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ViewAllComponent } from './view-all/view-all.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddProductComponent
+  },
+  {
+    path:"view",
+    component:ViewAllComponent
+  }
+
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AddProductComponent,
-    ViewAllComponent
+    ViewAllComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
